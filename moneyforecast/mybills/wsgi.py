5a34df022dcac9eed0197ada7de9fcac76157053
Anderson Santos <me@andersonsantos.info>
2022-04-14 06:18:17 +0200

    fix default settings on wsgi

--- moneyforecast/mybills/wsgi.py
+++ moneyforecast/mybills/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "minhascontas.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mybills.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
